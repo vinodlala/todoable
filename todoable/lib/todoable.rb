b9a7c1f5c25dd7367bee5890615fd7abb7bd7aef
Vinod Lala <vinod_lala@usa.net>
2019-01-27 11:34:06 -0500

    Starting up

---
+++ todoable/lib/todoable.rb
@@ -0,0 +1,11 @@
+require "todoable/version"
+require "rest-client"
+
+module Todoable
+  class Error < StandardError; end
+  # Your code goes here...
+
+  def self.hi
+    puts "Hello world!"
+  end
+end
