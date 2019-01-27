0518ea8dc53bdcf847d8622ae415028169b0fc0e
Vinod Lala <vinod_lala@usa.net>
2019-01-27 12:32:24 -0500

    Add more class methods

--- todoable/lib/todoable.rb
+++ todoable/lib/todoable.rb
@@ -8,4 +8,15 @@ module Todoable
   def self.hi
     puts "Hello world!"
   end
+
+  def self.bye
+    puts "Goodbye world!"
+  end
+
+  def self.get_token
+    puts "get_token start"
+
+
+    puts "get_token end"
+  end
 end
