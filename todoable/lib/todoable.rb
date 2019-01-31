2c7db0056e74b96c2c68e1748b11b59052234f92
Vinod Lala <vinod_lala@usa.net>
2019-01-30 20:54:59 -0500

    Clean up

--- todoable/lib/todoable.rb
+++ todoable/lib/todoable.rb
@@ -2,237 +2,29 @@ require "todoable/version"
 require "rest-client"
 
 module Todoable
-  class Error < StandardError; end
-
-  BASE_URL = "http://todoable.teachable.tech/api"
-
-  def self.hi
-    puts "Hello world!"
-  end
-
-  def self.bye
-    puts "Goodbye world!"
-  end
-
-  def self.get_token
-    response = RestClient::Request.execute(
-      method: :post,
-      url: "http://todoable.teachable.tech/api/authenticate",
-      user: "vinod_lala@usa.net",
-      password: "todoable"
-    )
-
-    response
-  end
-
-  def self.get_lists
-    response = Todoable::Client.new(:get, "/lists").execute
-
-    response
-  end
-
-  def self.post_list(name: "OLD")
-    post_params = {
-      list: {
-        name: name
-      }
-    }.to_json
-
-    # binding.pry
-
-    response = Todoable::Client.new(:post,
-                                    "/lists",
-                                    "",
-                                    post_params
-    ).execute
-
-    response
-  end
-
-  def self.get_list(list_id:)
-    response = Todoable::Client.new(:get, "/lists/#{list_id}").execute
-
-    response
-  end
-
-  def self.patch_list(list_id:, name:)
-    post_params = {
-      list: {
-        name: name
-      }
-    }.to_json
-
-    response = Todoable::Client.new(:patch,
-                                    "/lists/#{list_id}",
-                                    "",
-                                    post_params
-    ).execute
-
-    response
-  end
-
-  def self.delete_list(list_id:)
-    response = Todoable::Client.new(:delete,
-                                    "/lists/#{list_id}"
-    ).execute
-
-    response
-  end
-
-  def self.post_list_items(list_id:, name:)
-    post_params = {
-      item: {
-        name: name
-      }
-    }.to_json
-
-    response = Todoable::Client.new(:post,
-                                    "/lists/#{list_id}/items",
-                                    "",
-                                    post_params
-    ).execute
-
-    response
-  end
-
-  def self.finish_list_item(list_id:, item_id:)
-    response = Todoable::Client.new(:put,
-                                    "/lists/#{list_id}/items/#{item_id}/finish"
-    ).execute
-
-    response
-  end
-
-  def self.delete_list_item(list_id:, item_id:)
-    response = Todoable::Client.new(:delete,
-                                    "/lists/#{list_id}/items/#{item_id}"
-    ).execute
-
-    response
-  end
-
   class Client
+    BASE_URL = "http://todoable.teachable.tech/api"
+
     class NoResponseError < StandardError; end
+    class NoTokenError < StandardError; end
+    # RestClient has other errors
 
-    def initialize(method, endpoint = '', query_string = nil, payload = nil)
-      @method = method
+    attr_reader :token, :token_expires_at
 
-      @endpoint = endpoint
-
-      @query_string = query_string
-
-      @payload = payload
-    end
-
-    def execute
-      @response = RestClient::Request.execute(request_args)
-
-      if @response.is_a?(RestClient::Response)
-        return @response
-      else
-        raise NoResponseError
-      end
-    rescue NoResponseError,
-      RestClient::InternalServerError,
-      RestClient::BadRequest,
-      RestClient::ExceptionWithResponse,
-      RestClient::ResourceNotFound,
-      RestClient::Unauthorized,
-      Errno::ECONNREFUSED => e
-
-      puts "nope"
-      puts e
-    end
-
-    def this_be_token
-      response = Todoable.get_token
-
-      parsed_response = JSON.parse(response)
-
-      token = parsed_response["token"]
-
-      token
-    end
-
-    # def this_be_token_hc
-    #   token = "939cd8aa-456a-4c11-81be-4b01d2f5377a"
-    #
-    #   token
-    # end
-
-    def headers
-      out = {
-        'Content-Type': "application/json",
-        'Accept': "application/json",
-        # Authorization: "Token token='#{this_be_token}'"
-        # Authorization: "Token token='#{this_be_token_hc}'"
-
-        # Authorization: "Token token=\"#{this_be_token_hc}\""
-        # Authorization: "Token token=\"939cd8aa-456a-4c11-81be-4b01d2f5377a\""
-        Authorization: "Token token=\"#{this_be_token}\""
-      }
-
-      # This is a weird RestClient thing
-      if !@query_string.nil?
-        out.merge(params: @query_string)
-      else
-        out
-      end
-
-      out
-    end
-
-    def url
-      BASE_URL + @endpoint
-    end
-
-    def is_get_request?
-      @method == :get
-    end
-
-    def request_args
-      opts = { method: @method, url: url, headers: headers }
-
-      is_get_request? ? opts : opts.merge({ payload: @payload })
-    end
-  end
-
-  class ClientNew
-    attr_reader :token, :token_expires_at, :username, :password
-
-
-    def initialize(username: "vinod_lala@usa.net", password: "todoable")
+    def initialize(username:, password:)
       @username = username
 
       @password = password
 
       authenticate
-      # @options = {}
     end
 
     def authenticate
-      puts "in authenticate"
-      # @options[:basic_auth] = {
-      #   username: @username,
-      #   password: @password
-      # }
-
-      # response = self.class.post('/authenticate', @options)
-      # check_and_raise_errors(response)
-      # @token = response.parsed_response['token']
-      # tokens expire after 20 minutes
-
       token_info = get_token
-      puts "token_info"
-      puts token_info
 
       @token = token_info["token"]
 
-      puts "@token"
-      puts @token
-      @token_expires_at = token_info["token_expires_at"]
-      # use legit expiry in case it changes from 20 mins
-      @token_expiry = Time.now + (20 * 60)
+      @token_expires_at = Time.parse(token_info["expires_at"])
 
       self
     end
@@ -244,14 +36,15 @@ module Todoable
         user: @username,
         password: @password
       )
-      puts response
 
-      JSON.parse(response)
+      if response.is_a?(RestClient::Response)
+        JSON.parse(response)
+      else
+        raise NoResponseError
+      end
     end
 
     def rest_client_request(method:, path:, params: {})
-      puts "in rest_client_request"
-      puts headers
       response = RestClient::Request.execute(
         method: method,
         url: BASE_URL + path,
@@ -260,48 +53,36 @@ module Todoable
       )
 
       if response.is_a?(RestClient::Response)
-        return response
+        response
       else
         raise NoResponseError
       end
 
-      puts "leaving rest_client_request"
-      response
-    rescue NoResponseError,
-      RestClient::InternalServerError,
-      RestClient::BadRequest,
-      RestClient::ExceptionWithResponse,
-      RestClient::ResourceNotFound,
-      RestClient::Unauthorized,
-      Errno::ECONNREFUSED => e
-
-      puts "e.response"
-      e.response
-
-    # rescue => e
-    #   puts "rest_client_request in rescue"
-    #   puts e
-    #   check_and_raise_errors(e)
-
-      # rescue RestClient::ExceptionWithResponse => err
-      #   ErrorParser.parse_error(err.response)
+    # RestClient returns these errors, but they can be handled better
+    # rescue NoResponseError,
+    #   RestClient::InternalServerError,
+    #   RestClient::BadRequest,
+    #   RestClient::ExceptionWithResponse,
+    #   RestClient::ResourceNotFound,
+    #   RestClient::Unauthorized,
+    #   Errno::ECONNREFUSED => e
     end
 
     def get_lists
-      # check token
-      # response = Todoable::Client.new(:get, "/lists").execute
+      check_token
+
       response = rest_client_request(
         method: :get,
         path: "/lists"
       )
 
-      # check_and_raise_errors(response)
-
-      # return a hash
+      # Return a hash
       JSON.parse(response)
     end
 
     def post_list(name)
+      check_token
+
       post_params = {
         list: {
           name: name
@@ -314,25 +95,27 @@ module Todoable
         params: post_params
       )
 
-      # check_and_raise_errors(response)
-      # get a 422 UnprocessableEntity when it already exists
-
+      # Return a hash
+      # If a list with the name already exists, this is returned:
+      # {"name"=>["has already been taken"]}
       JSON.parse(response)
     end
 
     def get_list(list_id)
+      check_token
+
       response = rest_client_request(
         method: :get,
         path: "/lists/#{list_id}"
       )
 
-      # check_and_raise_errors(response)
-
-      # return a hash
+      # Return a hash
       JSON.parse(response)
     end
 
     def patch_list(list_id, name)
+      check_token
+
       post_params = {
         list: {
           name: name
@@ -345,27 +128,25 @@ module Todoable
         params: post_params
       )
 
-      # check_and_raise_errors(response)
-
-      puts response
-
+      # Returns "#{name of the list} updated"
       response
-      # JSON.parse(response)
     end
 
     def delete_list(list_id)
-      response = rest_client_request(method: :delete,
-                                     path: "/lists/#{list_id}"
+      check_token
+
+      response = rest_client_request(
+        method: :delete,
+        path: "/lists/#{list_id}"
       )
 
-      # check_and_raise_errors(response)
-      # 422 happens when it already exists
-
-      # JSON.parse(response)
+      # Returns an empty string
       response
     end
 
     def post_list_items(list_id, name)
+      check_token
+
       post_params = {
         item: {
           name: name
@@ -378,113 +159,53 @@ module Todoable
         params: post_params
       )
 
-      # check_and_raise_errors(response)
-
-      # JSON.parse(response)
-      response
+      # Returns a hash of the created list item only
+      # Multiple list items with the same name are allowed
+      JSON.parse(response)
     end
 
     def finish_list_item(list_id, item_id)
+      check_token
+
       response = rest_client_request(
         method: :put,
         path: "/lists/#{list_id}/items/#{item_id}/finish"
       )
 
-      # check_and_raise_errors(response)
-
+      # Returns "#{name of the list item} finished"
+      # 404 if item is already finished
       response
-      # JSON.parse(response)
     end
 
     def delete_list_item(list_id, item_id)
+      check_token
+
       response = rest_client_request(
         method: :delete,
         path: "/lists/#{list_id}/items/#{item_id}"
       )
 
-      # check_and_raise_errors(response)
-      # 422 happens when it already exists
-
-      # returns "" if it works
+      # Returns ""
+      # 404 if item is already finished
       response
-
-      # JSON.parse(response)
     end
-
-
-
-
-
-
-
 
     def check_token
       raise NoTokenError unless @token
-      authenticate if token_expired
+
+      authenticate if token_expired?
     end
 
-    def token_expired
-      Time.now > @token_expiry
-    end
-
-    def check_and_raise_errors(response)
-      puts "in check_and_raise_error"
-      puts "response error"
-      puts response
-      case response.code.to_i
-      when 200..300 then true
-      when 404 then raise NotFoundError
-      when 401 then raise UnauthorizedError
-      when 422
-        # errors = response.parsed_response['errors']
-        # raise UnprocessableError.new(errors: errors)
-        # raise Error.new(errors: errors)
-        raise UnprocessableError
-      else
-        raise StandardError, "Unknown error from Todoable: #{response}"
-      end
+    def token_expired?
+      @token_expires_at < Time.now.utc
     end
 
     def headers
-      out = {
+      {
         'Content-Type': "application/json",
         'Accept': "application/json",
-        # Authorization: "Token token='#{this_be_token}'"
-        # Authorization: "Token token='#{this_be_token_hc}'"
-
-        # Authorization: "Token token=\"#{this_be_token_hc}\""
-        # Authorization: "Token token=\"939cd8aa-456a-4c11-81be-4b01d2f5377a\""
-        # Authorization: "Token token=\"#{this_be_token}\""
-
         Authorization: "Token token=\"#{@token}\""
-        # 'Authorization': "#{@token}"
       }
-
-      # This is a weird RestClient thing
-      # if !@query_string.nil?
-      #   out.merge(params: @query_string)
-      # else
-      #   out
-      # end
-
-      puts "out"
-      puts out
-      out
-    end
-
-    class NoResponseError < StandardError
-    end
-
-    class NotFoundError < StandardError
-    end
-
-    class NoTokenError < StandardError
-    end
-
-    class UnauthorizedError < StandardError
-    end
-
-    class UnprocessableError < StandardError
     end
   end
 end
--- todoable/spec/todoable_spec.rb
+++ todoable/spec/todoable_spec.rb
@@ -3,37 +3,25 @@ RSpec.describe Todoable do
     expect(Todoable::VERSION).not_to be nil
   end
 
-  # it "does something useful" do
-  #   expect(false).to eq(true)
-  # end
-
-  describe ".get_lists" do
-    it "has a key called lists" do
-      response = JSON.parse(described_class.get_lists)
-
-      expect(response.key?("lists")).to eq true
-    end
-
-    it "has a value that is an array" do
-      response = JSON.parse(described_class.get_lists)
-
-      expect(response["lists"].is_a? Array).to eq true
-    end
-  end
-
-  describe "Todoable::ClientNew" do
+  describe "Todoable::Client" do
     describe "#get_lists" do
       it "has a key called lists" do
-        response = Todoable::ClientNew.new.get_lists
+        response = Todoable::Client.new.get_lists
 
         expect(response.key?("lists")).to eq true
       end
 
       it "has a value that is an array" do
-        response = Todoable::ClientNew.new.get_lists
+        response = Todoable::Client.new.get_lists
 
         expect(response["lists"].is_a? Array).to eq true
       end
+    end
+
+    describe "#get_list" do
+    end
+
+    describe "#post_list" do
     end
   end
 end
