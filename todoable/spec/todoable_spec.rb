3dfbd8d595b11de6ced9c740bf3bd197ad950ec6
Vinod Lala <vinod_lala@usa.net>
2019-01-31 19:51:53 -0500

    Add some tests

--- todoable/spec/todoable_spec.rb
+++ todoable/spec/todoable_spec.rb
@@ -4,24 +4,62 @@ RSpec.describe Todoable do
   end
 
   describe "Todoable::Client" do
-    describe "#get_lists" do
-      it "has a key called lists" do
-        response = Todoable::Client.new.get_lists
+    context "for an authenticated client" do
+      let(:username) { "vinod_lala@usa.net" }
+      let(:password) { "todoable" }
+      let(:client) { Todoable::Client.new(username: username,
+                                          password: password)}
 
-        expect(response.key?("lists")).to eq true
+      describe "#get_lists" do
+        it "has a key called lists" do
+          response = client.get_lists
+
+          expect(response.key?("lists")).to eq true
+        end
+
+        it "has a value that is an array" do
+          response = client.get_lists
+
+          expect(response["lists"].is_a? Array).to eq true
+        end
       end
 
-      it "has a value that is an array" do
-        response = Todoable::Client.new.get_lists
+      describe "#get_list" do
+        it "has a 'name' key" do
+          id = "710fea1c-fb14-4bc6-92cf-476ba9652b29"
 
-        expect(response["lists"].is_a? Array).to eq true
+          response = client.get_list(id)
+
+          expect(response.key?("name")).to eq true
+        end
+
+        it "has a 'name' value that is a string" do
+          id = "710fea1c-fb14-4bc6-92cf-476ba9652b29"
+
+          response = client.get_list(id)
+
+          expect(response["name"].is_a? String).to eq true
+        end
+
+        it "has an 'items' key" do
+          id = "710fea1c-fb14-4bc6-92cf-476ba9652b29"
+
+          response = client.get_list(id)
+
+          expect(response.key?("items")).to eq true
+        end
+
+        it "has an 'items' value that is an array" do
+          id = "710fea1c-fb14-4bc6-92cf-476ba9652b29"
+
+          response = client.get_list(id)
+
+          expect(response["items"].is_a? Array).to eq true
+        end
       end
-    end
 
-    describe "#get_list" do
-    end
-
-    describe "#post_list" do
+      describe "#post_list" do
+      end
     end
   end
 end
